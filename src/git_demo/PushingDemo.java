fee865eb467253e0b59e08ad8dd5a101a661d4c7
Prasanna-106 <prasanna9613@gmail.com>
2024-12-26 18:44:32 +0530

    Update PushingDemo.java
    Hi i have made a small change in code on github

--- src/git_demo/PushingDemo.java
+++ src/git_demo/PushingDemo.java
@@ -3,5 +3,6 @@ package git_demo;
 public class PushingDemo {
 	public static void main(String[] args) {
 		System.out.println("this is first change done in code");
+		System.out.println("i have made some chnages on github");
 	}
 }
